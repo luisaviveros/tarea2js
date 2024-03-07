b0b563c62ee6564527972e6ac5d59c5cb780fc7d
luisaviveros <luisaviveros73@gmail.com>
2024-03-07 08:56:31 -0500

    parte1tarea

---
+++ script.js
@@ -0,0 +1,22 @@
+const valor1 = prompt("Digite el primer valor");
+const valor2 = prompt("Digite el segundo valor");
+const valor3 = prompt("Digite el el tercer valor");
+
+const conversion1 = parseInt(valor1);
+const conversion2 = parseInt(valor2);
+const conversion3 = parseInt(valor3);
+
+const parrafo = document.querySelector("#parrafo");
+
+if (valor1 > valor2 && valor1 > valor3) {
+    parrafo.innerHTML = "el valor mas grande es " + valor1;
+}
+else if (valor2 > valor1 && valor2 > valor3) {
+    parrafo.innerHTML = "el valor mas grande es " + valor2;
+}
+else if (valor3 > valor1 && valor3 > valor2) {
+    parrafo.innerHTML = "el valor mas grande es " + valor3;
+}
+else {
+    parrafo.innerHTML = "los valores son igual es";
+}
